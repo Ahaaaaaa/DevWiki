22615bd8eb6a838151439b622a3cb1d804bfed02
bin.wang <bin.wang@zuolin.com>
2018-11-25 18:18:05 +0800

    init

---
+++ app.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import json
+from flask import Flask, jsonify, request
+from flask import render_template
+from db import REDIS
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    repos = [(key, json.loads(value)) for key, value in REDIS.hgetall('repos').items()]
+    # repos.sort(key=lambda x:x.star)
+    # print repos
+    # repos.sort(key=lambda x: x['star'], reverse=True)
+    # print repos
+    repos.sort(key=lambda x: x[1]['star'], reverse=True)
+    return render_template('index.html', repos=repos, keywords=REDIS.get('keywords').decode('utf-8'))
+
+
+@app.route('/del_repo', methods=['DELETE'])
+def del_repo():
+    if request.method == 'DELETE':
+        repo = request.args.get('repo', None)
+        if repo:
+            desc = REDIS.hget('repos', repo)
+            desc = json.loads(desc)
+            desc.update(is_show='0')
+            REDIS.hset('repos', repo, json.dumps(desc))
+            return jsonify({"status": True})
+
+
+if __name__ == '__main__':
+    app.run()
